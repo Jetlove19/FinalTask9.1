630e2a755e94e067c5d24a3e1be1b9a079f744e0
Jetlove19 <rbrorlove@gmail.com>
2023-03-02 14:10:22 +0300

    Добавьте файлы проекта.

---
+++ FinalTask9.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask9._1
+{
+    [Serializable]
+    public class StudentNotFoundException : Exception
+    {
+        public StudentNotFoundException()
+        {
+
+        }
+        public StudentNotFoundException(string message) : base(message)
+        {
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exceptions = { new ArgumentOutOfRangeException("Значение аргумента за пределами диапозона"), new FileLoadException("Ошибка скачивания файла"), new FileNotFoundException("Ошибка!Файл не найден"), new DivideByZeroException("Ошибка при делении на ноль!"), new StudentNotFoundException("Студент не найден") };
+
+            foreach (var item in exceptions)
+            {
+                try
+                {
+                    throw item;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+
+    }
+}
